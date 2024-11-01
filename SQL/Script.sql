cae9017638a84784b2208cdedf1890da3eecdc2c
Rafael Bini <rfabini1996@gmail.com>
2024-11-01 17:28:53 -0300

    second stage

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -1,25 +1,74 @@
 -- drop table users
 CREATE TABLE IF NOT EXISTS users
 (
-    id character varchar(240),
+    id serial primary key,
+	email varchar(240),
     emailed_token varchar(10),
-    user_id varchar(240),
-    CONSTRAINT pk_users PRIMARY KEY (id)
+    user_id varchar(240)
 )
+
+--insert into users values (default, 'rfabini1996@gmail.com', '', '')
 
 -- drop table members
 create table members (
-    id varchar(50),
+    id varchar(50) primary key,
     legacy_id varchar(20),
     name varchar(270),
     sex varchar(1),
     birth date,
-    user_id varchar(240),
-    CONSTRAINT pk_members PRIMARY KEY (id),
-    CONSTRAINT fk_member_user FOREIGN KEY (user_id)
-        REFERENCES users (id)
+    user_id int,
+    CONSTRAINT fk_member_user FOREIGN KEY (user_id) REFERENCES users (id)
 )
 
+-- drop table sacramentals
+create table sacramentals (
+	id int primary key,
+	date date,
+	sunday_of_the_month int,
+	user_id varchar(1)
+)
 
+-- drop table speeches
+create table speeches (
+	id serial primary key,
+	member_id varchar(50),
+	sacramental_id int,
+	minutes int,
+	topic varchar(200),
+	reference varchar(500),
+	message_copied boolean,
+	is_wild_card boolean,
+	wild_text varchar(120),
+	user_id int,
+	CONSTRAINT fk_speeches_member FOREIGN KEY (member_id) REFERENCES members (id),
+	CONSTRAINT fk_speeches_sacramental FOREIGN KEY (sacramental_id) REFERENCES sacramentals (id)
+)
 
+-- drop table invite_template
+create table invite_template (
+	id serial primary key,
+	invite_template varchar(900),
+	user_id int
+)
+
+-- drop view speeches_view;
+create view speeches_view as
+select 
+s.id as sacramental_id, 
+s.date as sacramental_date,
+s.sunday_of_the_month,
+p.id as speech_id,
+message_copied,
+member_id,
+m.name as member_name,
+p.is_wild_card,
+p.wild_text,
+topic,
+reference,
+minutes,
+p.user_id as speech_user_id,
+s.user_id
+from sacramentals s
+left join speeches p on (s.id = p.sacramental_id)
+left join members m on (m.id = p.member_id)
 
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const session = require('express-session');
 require('dotenv-safe').config()
 const db = require('./database.js');
 const jwt = require('jsonwebtoken');
 var cors = require('cors');
-const fs = require('fs'); //
 var nodemailer = require('nodemailer');
 const auth = require("./middlewares/auth");
 const getMembersData = require("./church_data_fetch");
 
-// EMAILER
+// #region EMAILER
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -17,8 +15,9 @@ var transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS
     }
 });
+// #endregion
 
-// EXPRESS
+// #region EXPRESS
 const app = express();
 var server;
 if (process.env.NODE_ENV == 'DEV') {
@@ -31,13 +30,15 @@ if (process.env.NODE_ENV == 'PROD') {
     }
     server = require('https').createServer(options, app);
 }
+// #endregion
 
-// MIDDLEWARES
+// #region MIDDLEWARES
 app.use(express.json());
 app.use(cors());
+// #endregion
 
-// FUNCTIONS
-function createToken() {
+// #region FUNCTIONS
+function createEmailToken() {
     var token = '';
     for (let i = 1; i <= 5; i++) {
         token += Math.floor(Math.random() * 10)
@@ -49,19 +50,20 @@ function createJwtToken(id) {
         expiresIn: 60 * 60 * 24 * 30 * 10 // 10 months in seconds
     });
 }
+// #endregion
 
-// Authentication
+// #region Authentication
 app.post('/send-email-token', async (req, res) => {
 
     // Check if has email
     if (!req.body.email) return res.status(400).json({ msg: "Email vazio" });
 
     // Check if user exists
-    var users = await db.get('users', req.body.email, req.body.email)
-    if (!users) return res.status(400).json({ msg: "Email não registrado" });
+    var users = await db.fetch('users', `email='${req.body.email}'`, '')
+    if (users.length <= 0) return res.status(400).json({ msg: "Email não registrado" });
 
 
-    const token = createToken();
+    const token = createEmailToken();
 
     // Enivar e registrar token 
     try {
@@ -78,12 +80,12 @@ app.post('/send-email-token', async (req, res) => {
                 console.log('Email error: ' + error);
             } else {
                 console.log('Email sent: ' + info.response);
-                db.update('users', { id: req.body.email, emailed_token: token }, req.body.email)
+                db.update('users', { id: users[0].id, emailed_token: token }, '')
             }
         });
 
         return res.status(200)
-            .json({ msg: "Email sent" });
+            .json({ msg: "Email sent to " + users[0].email });
     }
     catch (ex) {
         return res.status(400)
@@ -94,13 +96,14 @@ app.post('/send-email-token', async (req, res) => {
 })
 app.post('/validate-email-token', async (req, res) => {
 
-    var users = await db.fetch('users', `emailed_token='${req.body.token}'`, req.body.email,)
+    var users = await db.fetch('users', `emailed_token='${req.body.token}' and email='${req.body.email}'`, '',)
     if (users.length <= 0) return res.status(400).json({ msg: "Invalid token" });
 
-    return res.status(200).json({ msg: "Token accepted", jwt_token: createJwtToken(req.body.email) });
+    return res.status(200).json({ msg: "Token accepted", jwt_token: createJwtToken(users[0].id) });
 })
+// #endregion
 
-// MEMBERS KEEPER ENDPOINT
+// #region MEMBERS KEEPER ENDPOINT
 app.post('/load-members-data', auth, async (req, res) => {
     // VALIDATE CHURCH USER/PASSWORD
     const user = req.body.churchUser;
@@ -133,32 +136,157 @@ app.post('/load-members-data', auth, async (req, res) => {
     }
 
 })
+// #endregion
 
-// DATABASE ENDPOINTS
+// #region DATABASE ENDPOINTS
+
+app.get('/my-user', auth, async (req, res) => {
+    try {
+        var user = await db.get('users', req.userId, '');
+        return res.json(user)
+    }
+    catch (ex) {
+        return res.status(400).json(ex)
+    }
+
+})
 
 app.get('/:tableName/:id', auth, async (req, res) => {
-    res.json(await db.get(req.params.tableName, req.params.id, req.userId))
+    try {
+        res.json(await db.get(req.params.tableName, req.params.id, req.userId))
+    }
+    catch (ex) {
+        return res.status(400).json(ex)
+    }
 })
 
 app.delete('/:tableName/:id', auth, async (req, res) => {
-    res.json(await db.del(req.params.tableName, req.params.id, req.userId))
+    try {
+
+
+        res.json(await db.del(req.params.tableName, req.params.id, req.userId))
+    }
+    catch (ex) {
+        return res.status(400).json(ex)
+    }
 })
 
 app.post('/:tableName/delete-where', auth, async (req, res) => {
-    res.json(await db.deleteWhere(req.params.tableName, req.body.whereStr, req.userId))
+    try {
+
+
+        res.json(await db.deleteWhere(req.params.tableName, req.body.whereStr, req.userId))
+    }
+    catch (ex) {
+        return res.status(400).json(ex)
+    }
 })
 
 app.post('/:tableName/fetch', auth, async (req, res) => {
-    res.json(await db.fetch(req.params.tableName, req.body.whereStr, req.userId))
+    try {
+
+
+        res.json(await db.fetch(req.params.tableName, req.body.whereStr, req.userId))
+    }
+    catch (ex) {
+        return res.status(400).json(ex)
+    }
 })
 
 app.post('/:tableName/set', auth, async (req, res) => {
-    res.json(await db.set(req.params.tableName, req.body, req.userId))
+    try {
+
+
+        res.json(await db.set(req.params.tableName, req.body, req.userId))
+    }
+    catch (ex) {
+        return res.status(400).json(ex)
+    }
 })
 
 app.post('/:tableName/add', auth, async (req, res) => {
-    res.json(await db.add(req.params.tableName, req.body, req.userId))
+    try {
+
+
+        res.json(await db.add(req.params.tableName, req.body, req.userId))
+    }
+    catch (ex) {
+        return res.status(400).json(ex)
+    }
 })
+// #endregion
+
+// #region SPEECHES ENDPOINTS
+function getSundayOfTheMonth(date) {
+    // Create a new Date object from the input date
+    const targetDate = new Date(date);
+
+    // Get the year, month, and day of the target date
+    const year = targetDate.getFullYear();
+    const month = targetDate.getMonth();
+    const day = targetDate.getDate();
+
+    // Create a new Date object for the first day of the month
+    const firstDayOfMonth = new Date(year, month, 1);
+
+    // Calculate the day of the week for the first day of the month (0 = Sunday, 1 = Monday, ...)
+    const firstDayWeekDay = firstDayOfMonth.getDay();
+
+    // Calculate the difference in days between the first day of the month and the first Sunday
+    const daysToFirstSunday = (7 - firstDayWeekDay) % 7;
+
+    // Create a new Date object for the first Sunday of the month
+    const firstSunday = new Date(year, month, 1 + daysToFirstSunday);
+
+    // Calculate the difference in milliseconds between the target date and the first Sunday
+    const timeDiff = targetDate.getTime() - firstSunday.getTime();
+
+    // Calculate the number of weeks between the target date and the first Sunday
+    const weeksDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 7));
+
+    // Add 1 to the weeks difference to get the Sunday number of the month
+    const sundayOfTheMonth = weeksDiff + 1;
+
+    return sundayOfTheMonth;
+}
+app.post('/generate-sacramentals', auth, async (req, res) => {
+    try {
+
+        const curr_date = new Date();
+        var process_date = null;
+        for (let i = -800; i <= 1000; i++) {
+
+            process_date = new Date(curr_date.getTime() + (1000 * 60 * 60 * 24 * i))
+
+            // Sunday
+            if (process_date.getDay() == 0) {
+                var sacramentalId = process_date.toLocaleDateString('en-CA').replaceAll('-', '');
+                var exists = await db.get('sacramentals', sacramentalId, '');
+                if (exists) continue;
+
+                await db.set('sacramentals', { id: sacramentalId, date: process_date, sundayOfTheMonth: getSundayOfTheMonth(process_date) }, '')
+            }
+        }
+
+        return res.json({ msg: 'Sacramentals generated' })
+
+    }
+    catch (ex) {
+        return res.status(500).json(ex)
+    }
+})
+app.post('/speeches_view_fetch', auth, async (req, res) => {
+    try {
+
+        return res.json(await db.fetch('speeches_view', req.body.whereStr, ''))
+
+    }
+    catch (ex) {
+        console.log(ex)
+        return res.status(500).json(ex)
+    }
+})
+// #endregion
 
 
 // Iniciar o servidor
